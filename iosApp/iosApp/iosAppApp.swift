908b4a72450ef3b0eceee9ba91d4b86013dcde21
Beknur Patsaev <patsaev1994@mail.ru>
2024-12-06 23:57:25 +0300

    Failed attempt to add iOS

---
+++ iosApp/iosApp/iosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by Beknur Patsaev on 06.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

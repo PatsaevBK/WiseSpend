c4f5431cce3c5c23b9a89ea6e8f2acdf4cc9fbd7
Beknur Patsaev <patsaev1994@mail.ru>
2025-01-21 01:34:04 +0300

    add dto and AppApi

--- shared/src/commonMain/kotlin/info/javaway/spend_sense/storage/SettingsManager.kt
+++ shared/src/commonMain/kotlin/info/javaway/spend_sense/storage/SettingsManager.kt
@@ -8,7 +8,9 @@ import kotlinx.coroutines.flow.update
 
 class SettingsManager(private val settings: Settings) {
 
+    val serverUrl = "http://192.168.1.121:1337"
     private val THEME_KEY = "themeIsDark"
+    private val TOKEN_KEY = "TOKEN_KEY"
 
     var themeIsDark = settings[THEME_KEY, false]
         set(value) {
@@ -18,4 +20,8 @@ class SettingsManager(private val settings: Settings) {
 
     private val _themeIsDarkFlow = MutableStateFlow(themeIsDark)
     val themeIsDarkFlow = _themeIsDarkFlow.asStateFlow()
+
+    var token: String
+        set(value) = settings.putString(TOKEN_KEY, value)
+        get() = settings.getString(TOKEN_KEY, "")
 }
